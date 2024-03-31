0f7666081452d491c1a54273d788445890547770
David Rodriguez <davidrbacelar@gmail.com>
2024-03-31 23:56:39 +0200

    feat(web): display core error

--- crates/web/src/lib.rs
+++ crates/web/src/lib.rs
@@ -4,6 +4,6 @@ use wasm_bindgen::prelude::*;
 pub fn gq(query: &str, json: &str) -> String {
     match gq_core::entrypoint(query, json) {
         Ok(result) => serde_json::to_string_pretty(&result).expect("Failed to serialize result"),
-        Err(_) => "Invalid GQ".to_string(),
+        Err(e) => e.to_string(),
     }
 }
