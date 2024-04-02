801304f7f53491e459fbc5087c91d2832e8c1b3d
David Rodriguez <davidrbacelar@gmail.com>
2024-04-03 01:13:04 +0200

    chore: Add todo

--- crates/web/frontend/src/components/ui/slider.tsx
+++ crates/web/frontend/src/components/ui/slider.tsx
@@ -4,6 +4,8 @@ import * as React from "react";
 import * as SliderPrimitive from "@radix-ui/react-slider";
 import { cn } from "@/lib/utils";
 
+// TODO: Add disabled variant to Slider
+
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
