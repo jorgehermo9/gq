4fd5d13304520069a361a966bf92b6e08ada9a72
David Rodriguez <davidrbacelar@gmail.com>
2024-04-08 21:38:47 +0200

    chore(web): Add biome lint script

--- crates/web/frontend/src/hooks/useDebounce.tsx
+++ crates/web/frontend/src/hooks/useDebounce.tsx
@@ -1,16 +1,13 @@
 import { useEffect, useRef } from "react";
 
-const useDebounce = (
-	callback: () => void,
-	delay: number,
-	dependencies: any[],
-) => {
+const useDebounce = (callback: () => void, delay: number, dependencies: []) => {
 	const timer = useRef<NodeJS.Timeout | null>(null);
 
+	// biome-ignore lint/correctness/useExhaustiveDependencies: We want to avoid that the callback is called when the delay time changes
 	useEffect(() => {
 		timer.current && clearTimeout(timer.current);
 		timer.current = setTimeout(() => callback(), delay);
-	}, [callback, ...dependencies]);
+	}, [...dependencies]);
 };
 
 export default useDebounce;
