1f7c9f61aadc9d42e9f622be8dd626132c4888f0
David Rodriguez <davidrbacelar@gmail.com>
2024-04-14 17:54:47 +0200

    feat(web): Add drag and drop feature when importing file

--- crates/web/frontend/src/components/import-button/import-button.tsx
+++ crates/web/frontend/src/components/import-button/import-button.tsx
@@ -1,7 +1,6 @@
 import ActionButton from "@/components/action-button/action-button";
 import type FileType from "@/model/file-type";
-import { DialogTrigger } from "@radix-ui/react-dialog";
-import { FileUp } from "lucide-react";
+import { File, FileUp, Trash } from "lucide-react";
 import { useState } from "react";
 import { toast } from "sonner";
 import { Button } from "../ui/button";
@@ -9,12 +8,14 @@ import {
 	Dialog,
 	DialogContent,
 	DialogDescription,
-	DialogFooter,
 	DialogHeader,
 	DialogTitle,
+	DialogTrigger,
 } from "../ui/dialog";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { Separator } from "../ui/separator";
+import { formatBytes } from "@/lib/utils";
 
 interface Props {
 	fileType: FileType;
@@ -25,18 +26,19 @@ interface Props {
 const ImportButton = ({ fileType, onImportFile, hidden = false }: Props) => {
 	const [open, setOpen] = useState(false);
 	const [url, setUrl] = useState("");
+	const [file, setFile] = useState<File>();
 
-	const handleImportFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
-		const file = e.target.files?.[0];
+	const handleImportFile = async () => {
 		if (!file) return;
 		const reader = new FileReader();
 		reader.onload = () => {
 			const content = reader.result as string;
 			onImportFile(content);
 			toast.success("File imported successfully!");
+			setOpen(false);
+			setFile(undefined);
 		};
 		reader.readAsText(file);
-		e.target.value = "";
 	};
 
 	const handleImportUrl = () => {
@@ -62,9 +64,19 @@ const ImportButton = ({ fileType, onImportFile, hidden = false }: Props) => {
 			});
 	};
 
+	const handleDrop = (e: React.DragEvent<HTMLLabelElement>) => {
+		e.preventDefault();
+		const file = e.dataTransfer.files[0];
+		if (file) {
+			setFile(file);
+		}
+	};
+
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		if (url) {
+		if (file) {
+			handleImportFile();
+		} else if (url) {
 			handleImportUrl();
 		}
 	};
@@ -80,18 +92,24 @@ const ImportButton = ({ fileType, onImportFile, hidden = false }: Props) => {
 					<FileUp className="w-3.5 h-3.5" />
 				</ActionButton>
 			</DialogTrigger>
-			<DialogContent className="w-[16vw] min-w-96 max-w-sccreen">
+			<DialogContent className="w-[28vw] min-w-96 max-w-sccreen">
 				<DialogHeader>
 					<DialogTitle>Import file</DialogTitle>
 					<DialogDescription>
-						Import a valid file to the editor via URL or local file upload
+						Import a valid file to the editor either from a URL or a local file.
 					</DialogDescription>
 				</DialogHeader>
 				<form onSubmit={handleSubmit} autoComplete="off">
-					<div className="flex flex-col gap-4">
+					<div>
 						<div>
-							<Label htmlFor="url-import">From URL</Label>
+							<Label
+								htmlFor="url-import"
+								variant={file !== undefined ? "disabled" : "default"}
+							>
+								From URL
+							</Label>
 							<Input
+								disabled={file !== undefined}
 								id="url-import"
 								type="url"
 								placeholder="Enter URL"
@@ -101,18 +119,62 @@ const ImportButton = ({ fileType, onImportFile, hidden = false }: Props) => {
 							/>
 						</div>
 
-						<Label
-							htmlFor="file-import"
-							className="bg-muted rounded-md border border-foreground border-dashed w-full h-40 cursor-pointer"
-						>
-							<input
-								id="file-import"
-								hidden
-								type="file"
-								accept=".json,.gq"
-								onChange={handleImportFile}
-							/>
-						</Label>
+						<Separator />
+
+						<div className="w-full">
+							<span className="text-sm">From local file</span>
+							<div className="w-full h-full relative group">
+								<Label
+									htmlFor="file-import"
+									className="flex items-center rounded-md justify-center border-2 border-muted border-dashed w-full h-40 cursor-pointer mt-2"
+									onDrop={handleDrop}
+									onDragOver={(e) => e.preventDefault()}
+								>
+									<input
+										disabled={url.length > 0}
+										id="file-import"
+										hidden
+										type="file"
+										accept={`.${fileType}`}
+										onChange={(e) => {
+											setFile(e.target.files?.[0]);
+											e.target.value = "";
+										}}
+									/>
+									{file ? (
+										<div className="flex flex-col items-center justify-center gap-2 w-full h-full relative">
+											<File className="w-6 h-6" />
+											<span className="text-center leading-5 font-semibold">
+												{file.name}
+											</span>
+											<span className="text-xs">{formatBytes(file.size)}</span>
+										</div>
+									) : (
+										<span className="w-1/2 text-center leading-5">
+											Drag and drop a file here or click to select a file
+										</span>
+									)}
+								</Label>
+								{file && (
+									<div
+										className="absolute inset-0 z-20 rounded-md w-full h-full flex items-center justify-center 
+									bg-black bg-opacity-70 opacity-0 invisible group-hover:opacity-100 
+									group-hover:visible transition-all duration-200 delay-200"
+									>
+										<ActionButton
+											className="p-2"
+											onClick={(e) => {
+												setFile(undefined);
+												e.stopPropagation();
+											}}
+											description="Remove file"
+										>
+											<Trash className="w-5 h-5" />
+										</ActionButton>
+									</div>
+								)}
+							</div>
+						</div>
 					</div>
 
 					<div className="flex justify-between mt-12">
