4e6595643da899e7fd1d4cbce70b15a94ebfe9ec
Jorge Hermo <jorgehermogonzalez@gmail.com>
2024-04-19 18:51:39 +0200

    chore(core): Add some todos for the QueryArgument

--- crates/core/src/query/query_argument.rs
+++ crates/core/src/query/query_argument.rs
@@ -15,3 +15,9 @@ pub struct QueryArgument<'a> {
     key: QueryKey<'a>,
     value: QueryArgumentValue<'a>,
 }
+
+// TODO: create an filter method that returns a boolean...
+// maybe the QueryKey::inspect should return a reference to que inspected
+// value, as we do not need ownership, only to check equality to the inspected
+// value... The apply should receive a Vec<Value> and inspect each value
+// with the query key
