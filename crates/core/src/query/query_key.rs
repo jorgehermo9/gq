7f165538292b2de53f71d362b1797cbd1568e7ca
Jorge Hermo <jorgehermogonzalez@gmail.com>
2024-05-10 19:59:25 +0200

    refactor(core): split `QueryKey::do_inspect` in multiple functions

--- crates/core/src/query/query_key.rs
+++ crates/core/src/query/query_key.rs
@@ -105,90 +105,112 @@ impl<'a> QueryKey<'a> {
         parent_arguments: &QueryArguments<'a>,
         context: &Context<'a>,
     ) -> Result<Cow<'b, Value>, InternalError<'a>> {
-        // TODO: split in do_inspect_object, do_inspect_array and do_inspect_primitive
-
         let result = match value {
             Cow::Owned(Value::Object(_)) | Cow::Borrowed(Value::Object(_)) => {
-                if !parent_arguments.0.is_empty() {
-                    // TODO: throw an error here or log a warning?
-                    // in my opinion we should fail
-                    return Err(InternalError::NonFiltrableValue(context.path().clone()));
-                }
-
-                let Some((atomic_query_key, rest)) = keys.split_first() else {
-                    return Ok(value);
-                };
-
-                let raw_key = atomic_query_key.key();
-                let arguments = atomic_query_key.arguments();
-                let new_context = context.push_raw_key(raw_key);
-
-                let current = match value {
-                    Cow::Owned(Value::Object(mut object)) => object
-                        .get_mut(raw_key.0.as_ref())
-                        .map(Value::take)
-                        .map(Cow::Owned),
-                    Cow::Borrowed(Value::Object(object)) => object
-                        // TODO: implement Borrow so we can do .get(raw_key)
-                        .get(raw_key.0.as_ref())
-                        .map(Cow::Borrowed),
-                    _ => unreachable!("In this match branch there are only Value::Object variants"),
-                }
-                .ok_or(InternalError::KeyNotFound(new_context.path().clone()))?;
-
-                Self::do_inspect(current, rest, arguments, &new_context)?
+                Self::do_inspect_object(value, keys, parent_arguments, context)?
             }
             Cow::Owned(Value::Array(_)) | Cow::Borrowed(Value::Array(_)) => {
-                let array_context = context.enter_array();
-
-                // TODO: think if there is a better way to do this, but I think this is the best we can do
-                let array_iter: Box<dyn Iterator<Item = Cow<Value>>> = match value {
-                    Cow::Owned(Value::Array(array)) => Box::new(array.into_iter().map(Cow::Owned)),
-                    Cow::Borrowed(Value::Array(array)) => Box::new(array.iter().map(Cow::Borrowed)),
-                    _ => unreachable!("In this match branch there are only Value::Array variants"),
-                };
-
-                let result = array_iter
-                    .enumerate()
-                    .map(|(index, item)| (array_context.push_index(index), item))
-                    .filter(|(item_context, item)| parent_arguments.satisfies(item, item_context))
-                    .map(|(item_context, item)| {
-                        let default_query_arguments = QueryArguments::default();
-                        let arguments_to_propagate = match item {
-                            // Only propagate parent_arguments if the child is an array
-                            Cow::Owned(Value::Array(_)) | Cow::Borrowed(Value::Array(_)) => {
-                                parent_arguments
-                            }
-                            _ => &default_query_arguments,
-                        };
-                        Self::do_inspect(item, keys, arguments_to_propagate, &item_context)
-                    })
-                    .flat_map(|result| {
-                        result
-                            .map_err(|error| {
-                                let array_error = InternalError::InsideArray(
-                                    Box::new(error),
-                                    array_context.path().clone(),
-                                );
-                                log::warn!("{array_error}");
-                            })
-                            .ok()
-                    })
-                    // We have to own the values if we want to return a Value::Array
-                    .map(Cow::into_owned)
-                    .collect();
-                Cow::Owned(Value::Array(result))
+                Self::do_inspect_array(value, keys, parent_arguments, context)
             }
-            value => {
-                if !parent_arguments.0.is_empty() {
-                    return Err(InternalError::NonFiltrableValue(context.path().clone()));
-                }
-                if !keys.is_empty() {
-                    return Err(InternalError::NonIndexableValue(context.path().clone()));
-                }
-                value
-            }
+            value => Self::do_inspect_primitive(value, keys, parent_arguments, context)?,
         };
         Ok(result)
+    }
+
+    pub fn do_inspect_object<'b>(
+        value: Cow<'b, Value>,
+        keys: &'a [AtomicQueryKey<'a>],
+        parent_arguments: &QueryArguments<'a>,
+        context: &Context<'a>,
+    ) -> Result<Cow<'b, Value>, InternalError<'a>> {
+        if !parent_arguments.0.is_empty() {
+            // TODO: throw an error here or log a warning?
+            // in my opinion we should fail
+            return Err(InternalError::NonFiltrableValue(context.path().clone()));
+        }
+
+        let Some((atomic_query_key, rest)) = keys.split_first() else {
+            return Ok(value);
+        };
+
+        let raw_key = atomic_query_key.key();
+        let arguments = atomic_query_key.arguments();
+        let new_context = context.push_raw_key(raw_key);
+
+        let current = match value {
+            Cow::Owned(Value::Object(mut object)) => object
+                .get_mut(raw_key.0.as_ref())
+                .map(Value::take)
+                .map(Cow::Owned),
+            Cow::Borrowed(Value::Object(object)) => object
+                // TODO: implement Borrow so we can do .get(raw_key)
+                .get(raw_key.0.as_ref())
+                .map(Cow::Borrowed),
+            _ => unreachable!("In this match branch there are only Value::Object variants"),
+        }
+        .ok_or(InternalError::KeyNotFound(new_context.path().clone()))?;
+
+        Self::do_inspect(current, rest, arguments, &new_context)
+    }
+    pub fn do_inspect_array<'b>(
+        value: Cow<'b, Value>,
+        keys: &'a [AtomicQueryKey<'a>],
+        parent_arguments: &QueryArguments<'a>,
+        context: &Context<'a>,
+    ) -> Cow<'b, Value> {
+        let array_context = context.enter_array();
+
+        // TODO: think if there is a better way to do this, but I think this is the best we can do
+        let array_iter: Box<dyn Iterator<Item = Cow<Value>>> = match value {
+            Cow::Owned(Value::Array(array)) => Box::new(array.into_iter().map(Cow::Owned)),
+            Cow::Borrowed(Value::Array(array)) => Box::new(array.iter().map(Cow::Borrowed)),
+            _ => unreachable!("In this match branch there are only Value::Array variants"),
+        };
+
+        let result = array_iter
+            .enumerate()
+            .map(|(index, item)| (array_context.push_index(index), item))
+            .filter(|(item_context, item)| parent_arguments.satisfies(item, item_context))
+            .map(|(item_context, item)| {
+                let default_query_arguments = QueryArguments::default();
+                let arguments_to_propagate = match item {
+                    // Only propagate parent_arguments if the child is an array
+                    Cow::Owned(Value::Array(_)) | Cow::Borrowed(Value::Array(_)) => {
+                        parent_arguments
+                    }
+                    _ => &default_query_arguments,
+                };
+                Self::do_inspect(item, keys, arguments_to_propagate, &item_context)
+            })
+            .flat_map(|result| {
+                result
+                    .map_err(|error| {
+                        let array_error = InternalError::InsideArray(
+                            Box::new(error),
+                            array_context.path().clone(),
+                        );
+                        log::warn!("{array_error}");
+                    })
+                    .ok()
+            })
+            // We have to own the values if we want to return a Value::Array
+            .map(Cow::into_owned)
+            .collect();
+        Cow::Owned(Value::Array(result))
+    }
+
+    pub fn do_inspect_primitive<'b>(
+        value: Cow<'b, Value>,
+        keys: &'a [AtomicQueryKey<'a>],
+        parent_arguments: &QueryArguments<'a>,
+        context: &Context<'a>,
+    ) -> Result<Cow<'b, Value>, InternalError<'a>> {
+        if !parent_arguments.0.is_empty() {
+            return Err(InternalError::NonFiltrableValue(context.path().clone()));
+        }
+        if !keys.is_empty() {
+            return Err(InternalError::NonIndexableValue(context.path().clone()));
+        }
+        Ok(value)
     }
 }
