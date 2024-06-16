dd84d0390e1b9211907b6a461eadc9c0745a66e2
Jorge Hermo <jorgehermogonzalez@gmail.com>
2024-06-16 18:02:43 +0200

    test(core): Add tests for lexer parsing

--- crates/core/src/lexer.rs
+++ crates/core/src/lexer.rs
@@ -58,7 +58,8 @@ pub enum Token {
     #[regex(r"-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?", |lex| lex.slice().parse::<f64>().unwrap())]
     Number(f64),
     #[regex(r#"(?:"(?:[^"]|\\")*")|(?:'(?:[^']|\\')*')"#, |lex| {
-        unescape::unescape(&lex.slice()[1..lex.slice().len() - 1]).unwrap()
+        // TODO: handle the unquote error with custom lexer errors
+        enquote::unquote(lex.slice()).expect("Error while unquoting")
     }
     )]
     String(String),
@@ -92,5 +93,112 @@ impl Display for Token {
             Token::Null => write!(f, "null"),
             Token::EOF => write!(f, "EOF"),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case::l_brace("{", Token::LBrace)]
+    #[case::r_brace("}", Token::RBrace)]
+    #[case::l_paren("(", Token::LParen)]
+    #[case::r_paren(")", Token::RParen)]
+    #[case::dot(".", Token::Dot)]
+    #[case::colon(":", Token::Colon)]
+    #[case::comma(",", Token::Comma)]
+    #[case::equal("=", Token::Equal)]
+    #[case::not_equal("!=", Token::NotEqual)]
+    #[case::greater(">", Token::Greater)]
+    #[case::greater_equal(">=", Token::GreaterEqual)]
+    #[case::less("<", Token::Less)]
+    #[case::less_equal("<=", Token::LessEqual)]
+    #[case::tilde("~", Token::Tilde)]
+    #[case::not_tilde("!~", Token::NotTilde)]
+    #[case::true_token("true", Token::Bool(true))]
+    #[case::false_token("false", Token::Bool(false))]
+    #[case::null("null", Token::Null)]
+    fn simple_token_parses(#[case] input: &str, #[case] expected: Token) {
+        let mut lexer = Token::lexer(input);
+
+        let token_result = lexer.next().expect("No token found");
+        let token = token_result.expect("Error parsing token");
+
+        assert_eq!(token, expected);
+    }
+
+    #[rstest]
+    #[case::positive("5", 5.0)]
+    #[case::negative("-5", -5.0)]
+    #[case::float("5.5", 5.5)]
+    #[case::negative_float("-5.5", -5.5)]
+    #[case::float_with_exponent("5.5e5", 5.5e5)]
+    #[case::float_with_negative_exponent("5.5e-5", 5.5e-5)]
+    #[case::float_with_positive_exponent("5.5e+5", 5.5e5)]
+    #[case::float_with_uppercase_exponent("5.5E5", 5.5e5)]
+    #[case::float_with_uppercase_positive_exponent("5.5E+5", 5.5e5)]
+    #[case::float_with_uppercase_negative_exponent("5.5E-5", 5.5e-5)]
+    fn number_token_parses(#[case] input: &str, #[case] expected: f64) {
+        let expected = Token::Number(expected);
+        let mut lexer = Token::lexer(input);
+
+        let token_result = lexer.next().expect("No token found");
+        let token = token_result.expect("Error parsing token");
+
+        assert_eq!(token, expected);
+    }
+
+    #[rstest]
+    #[case::simple("key")]
+    #[case::with_underscore("key_with_underscore")]
+    #[case::with_numbers("key_with_123_numbers")]
+    #[case::with_dash("key-with-dash")]
+    #[case::with_dash_and_underscore("key-with-dash_and_underscore")]
+    #[case::with_caps("KeyWithCaps")]
+    #[case::starting_with_underscore("_key")]
+    fn identifier_token_parses(#[case] input: &str) {
+        let expected = Token::Identifier(input.to_string());
+        let mut lexer = Token::lexer(input);
+
+        let token_result = lexer.next().expect("No token found");
+        let token = token_result.expect("Error parsing token");
+
+        assert_eq!(token, expected);
+    }
+
+    #[rstest]
+    #[case::simple(r#""JavaScript""#, "JavaScript")]
+    #[case::simple_with_single_quotes(r#"'JavaScript'"#, "JavaScript")]
+    #[case::with_space(r#""Java Script""#, "Java Script")]
+    #[case::with_double_commas(r#""Java\"Script""#, "Java\"Script")]
+    #[case::with_single_commas(r#"'Java\'Script'"#, "Java\'Script")]
+    #[case::single_quoted_with_double_commas(r#"'Java\"Script'"#, "Java\"Script")]
+    #[case::double_quoted_with_single_commas(r#""Java\'Script""#, "Java\'Script")]
+    #[case::newline(r#""Java\nScript""#, "Java\nScript")]
+    #[case::tab(r#""Java\tScript""#, "Java\tScript")]
+    #[case::backslash(r#""Java\\Script""#, "Java\\Script")]
+    #[case::backslash_and_quote(r#""Java\\\"Script""#, "Java\\\"Script")]
+    #[case::mixed(r#""Jav\r\n\ta\\\"Scri\"pt\n""#, "Jav\r\n\ta\\\"Scri\"pt\n")]
+    fn string_token_parses(#[case] input: &str, #[case] expected: &str) {
+        let expected = Token::String(expected.to_string());
+        let mut lexer = Token::lexer(input);
+
+        let token_result = lexer.next().expect("No token found");
+        let token = token_result.expect("Error while parsing token");
+
+        assert_eq!(token, expected);
+    }
+
+    // TODO: change the lexer so an `Err` variant is returned instead of panicking
+    #[test]
+    #[should_panic]
+    fn string_token_parse_unquote_fails_when_malformed_escape() {
+        let input = r#""Java\\"Script""#;
+        let mut lexer = Token::lexer(input);
+
+        let token_result = lexer.next().expect("No token found");
+        token_result.expect("Error while parsing token");
     }
 }
