1f3467c4f154171593ea9bb328f0e562a82da2c5
David Rodriguez <davidrbacelar@gmail.com>
2024-06-17 20:17:03 +0200

    chore(web): Add loading animation to star counter

--- crates/web/frontend/src/components/header/star-count.tsx
+++ crates/web/frontend/src/components/header/star-count.tsx
@@ -2,6 +2,7 @@ import { cn, formatNumber } from "@/lib/utils";
 import { Github, Star } from "lucide-react";
 import { type MouseEvent, useCallback, useEffect, useState } from "react";
 import styles from "./header.module.css";
+import { Loader } from "../ui/sonner";
 
 interface Props {
 	className?: string;
@@ -10,6 +11,7 @@ interface Props {
 const StarCount = ({ className }: Props) => {
 	const [stars, setStars] = useState<string>();
 	const [clicked, setClicked] = useState<boolean>(false);
+	const [fetching, setFetching] = useState<boolean>(true);
 
 	const handleClick = useCallback((e: MouseEvent<HTMLAnchorElement>) => {
 		setClicked(true);
@@ -24,7 +26,8 @@ const StarCount = ({ className }: Props) => {
 		fetch("https://api.github.com/repos/jorgehermo9/gq")
 			.then((res) => res.json())
 			.then((data) => setStars(formatNumber(data.stargazers_count)))
-			.catch(() => setStars("N/A"));
+			.catch(() => setStars("N/A"))
+			.finally(() => setFetching(false));
 	}, []);
 
 	return (
@@ -32,11 +35,16 @@ const StarCount = ({ className }: Props) => {
 			href="https://github.com/jorgehermo9/gq"
 			onClick={handleClick}
 			className={cn(styles.starCount, className)}
-			data-visible={!!stars}
 		>
 			<Github className="w-4 h-4 mr-2" />
-			<span className="text-sm font-medium mr-1">{stars}</span>
-			<Star className="w-3 h-3 text-accent data-[visible=false]:opacity-0 data-[visible=true]:opacity-1 transition-opacity" />
+			{fetching ? (
+				<Loader />
+			) : (
+				<div className="animate-in fade-in duration-300 flex items-center">
+					<span className="text-sm font-medium mr-1">{stars}</span>
+					<Star className="w-3 h-3 text-accent data-[visible=false]:opacity-0 data-[visible=true]:opacity-1 transition-opacity" />
+				</div>
+			)}
 		</a>
 	);
 };
