e0f4be63456d67ef19b117ff71fc2562d351b50b
Jorge Hermo <jorgehermogonzalez@gmail.com>
2024-09-14 17:31:26 +0200

    test(core): Add more tests for field aliasing

--- crates/core/tests/field_aliasing/mod.rs
+++ crates/core/tests/field_aliasing/mod.rs
@@ -5,14 +5,14 @@ use serde_json::{json, Value};
 use crate::fixtures::ai_models;
 
 #[rstest]
-fn primitive_property(ai_models: Value) {
+fn simple(ai_models: Value) {
     let query: Query = r#"{
-      id: identifier
+        id: identifier
     }"#
     .parse()
     .unwrap();
     let expected = json!({
-      "identifier": "AI-Models"
+        "identifier": "AI-Models"
     });
 
     let result = query.apply(ai_models).unwrap();
@@ -21,18 +21,148 @@ fn primitive_property(ai_models: Value) {
 }
 
 #[rstest]
-fn complex(ai_models: Value) {
+fn nested_property(ai_models: Value) {
     let query: Query = r#"{
-      models.name: modelNames
+        models.name: modelNames
     }"#
     .parse()
     .unwrap();
     let expected = json!({
-      "modelNames": [
-        "GPT-4O",
-        "Claude",
-        "LLAMA"
-      ]
+        "modelNames": [
+            "GPT-4O",
+            "Claude",
+            "LLAMA"
+        ]
+    });
+
+    let result = query.apply(ai_models).unwrap();
+
+    assert_eq!(result, expected);
+}
+
+#[rstest]
+fn multiple_properties(ai_models: Value) {
+    let query: Query = r#"{
+        id: identifier
+        models.name: modelNames
+    }"#
+    .parse()
+    .unwrap();
+    let expected = json!({
+        "identifier": "AI-Models",
+        "modelNames": [
+            "GPT-4O",
+            "Claude",
+            "LLAMA"
+        ]
+    });
+
+    let result = query.apply(ai_models).unwrap();
+
+    assert_eq!(result, expected);
+}
+
+#[rstest]
+fn outer(ai_models: Value) {
+    let query: Query = r#"{
+        models: MyAiModels {
+            name
+            openSource
+            score
+        }
+    }"#
+    .parse()
+    .unwrap();
+    let expected = json!({
+        "MyAiModels": [
+            {
+                "name": "GPT-4O",
+                "openSource": false,
+                "score": 71.49
+            },
+            {
+                "name": "Claude",
+                "openSource": false,
+                "score": null
+            },
+            {
+                "name": "LLAMA",
+                "openSource": true,
+                "score": 88.7
+            }
+        ]
+    });
+
+    let result = query.apply(ai_models).unwrap();
+
+    assert_eq!(result, expected);
+}
+
+#[rstest]
+fn inner(ai_models: Value) {
+    let query: Query = r#"{
+        models {
+            name: modelName
+            openSource: isOSS
+            score: modelScore
+        }
+    }"#
+    .parse()
+    .unwrap();
+    let expected = json!({
+        "models": [
+            {
+                "modelName": "GPT-4O",
+                "isOSS": false,
+                "modelScore": 71.49
+            },
+            {
+                "modelName": "Claude",
+                "isOSS": false,
+                "modelScore": null
+            },
+            {
+                "modelName": "LLAMA",
+                "isOSS": true,
+                "modelScore": 88.7
+            }
+        ]
+    });
+
+    let result = query.apply(ai_models).unwrap();
+
+    assert_eq!(result, expected);
+}
+
+#[rstest]
+fn outer_and_inner(ai_models: Value) {
+    let query: Query = r#"{
+        models: MyAiModels {
+            name: modelName
+            openSource: isOSS
+            score: modelScore
+        }
+    }"#
+    .parse()
+    .unwrap();
+    let expected = json!({
+        "MyAiModels": [
+            {
+                "modelName": "GPT-4O",
+                "isOSS": false,
+                "modelScore": 71.49
+            },
+            {
+                "modelName": "Claude",
+                "isOSS": false,
+                "modelScore": null
+            },
+            {
+                "modelName": "LLAMA",
+                "isOSS": true,
+                "modelScore": 88.7
+            }
+        ]
     });
 
     let result = query.apply(ai_models).unwrap();
