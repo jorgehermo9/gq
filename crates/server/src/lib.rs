3b9a5b856926ca837866ee9119d3c806997187da
Jorge Hermo <jorgehermogonzalez@gmail.com>
2024-10-27 12:39:32 +0100

    Revert "feat(server): background cleanup task (#51)"
    This reverts commit 2bbdf30d47762a8149b287a94579eb0072f71c0d.

--- crates/server/src/lib.rs
+++ crates/server/src/lib.rs
@@ -6,7 +6,6 @@ pub mod api;
 pub mod app_state;
 pub mod model;
 pub mod services;
-pub mod tasks;
 
 pub fn app(db_connection: PgPool, max_share_expiration_time_secs: i64) -> Router {
     let app_state = AppState::new(db_connection, max_share_expiration_time_secs);
