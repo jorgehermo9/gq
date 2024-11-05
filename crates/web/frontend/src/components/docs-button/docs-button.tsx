32dda6664e7e4172b095b69ce2e79a374239498e
David Rodriguez <davidrbacelar@gmail.com>
2024-11-05 21:09:23 +0100

    chore: Improve link buttons

--- crates/web/frontend/src/components/docs-button/docs-button.tsx
+++ crates/web/frontend/src/components/docs-button/docs-button.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { BookOpen } from "lucide-react";
 import { type MouseEvent, useCallback } from "react";
 import ActionButton from "../action-button/action-button";
@@ -15,13 +16,18 @@ export const DocsButton = ({ className }: Props) => {
 	return (
 		<ActionButton
 			description="Check the GQ documentation"
-			className={className}
+			className="h-full"
 			variant="subtle"
 			onClick={handleClick}
 		>
-			<a href="/docs" rel="noreferrer" target="_blank" className="flex items-center select-none">
+			<a
+				href="/docs"
+				rel="noreferrer"
+				target="_blank"
+				className={cn("flex items-center select-none", className)}
+			>
 				<BookOpen className="w-3.5 h-3.5 mr-2" />
-				<span className="text-xxs font-normal mt-[2px]">Docs</span>
+				<span className="text-xxs font-normal mt-[1px]">Docs</span>
 			</a>
 		</ActionButton>
 	);
--- crates/web/frontend/src/components/star-count/star-count.tsx
+++ crates/web/frontend/src/components/star-count/star-count.tsx
@@ -1,6 +1,6 @@
-import { formatNumber } from "@/lib/utils";
+import { cn, formatNumber } from "@/lib/utils";
 import { Github, Star } from "lucide-react";
-import { type MouseEvent, useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ActionButton from "../action-button/action-button";
 import { Loader } from "../ui/sonner";
 
@@ -12,11 +12,6 @@ const StarCount = ({ className }: Props) => {
 	const [stars, setStars] = useState<string>();
 	const [fetching, setFetching] = useState<boolean>(true);
 
-	const handleClick = useCallback((e: MouseEvent<HTMLButtonElement>) => {
-		window.open("https://github.com/jorgehermo9/gq", "_blank");
-		e.preventDefault();
-	}, []);
-
 	useEffect(() => {
 		fetch("https://api.github.com/repos/jorgehermo9/gq")
 			.then((res) => res.json())
@@ -26,18 +21,18 @@ const StarCount = ({ className }: Props) => {
 	}, []);
 
 	return (
-		<ActionButton
-			description="Check the GQ Github repository"
-			className={className}
-			variant="subtle"
-			onClick={handleClick}
-		>
-			<a href="https://github.com/jorgehermo9/gq" className="flex items-center select-none">
+		<ActionButton description="Check the GQ Github repository" variant="subtle" className="h-full">
+			<a
+				href="https://github.com/jorgehermo9/gq"
+				rel="noreferrer"
+				target="_blank"
+				className={cn("flex items-center select-none", className)}
+			>
 				<Github className="w-3.5 h-3.5 mr-2" />
 				{fetching ? (
 					<Loader />
 				) : (
-					<div className="animate-in fade-in duration-300 flex items-center mt-[2px]">
+					<div className="animate-in fade-in duration-300 flex items-center mt-[1px]">
 						<span className="text-xxs font-normal mr-1">{stars}</span>
 						<Star className="w-2 h-2 mb-[1px] text-accent data-[visible=false]:opacity-0 data-[visible=true]:opacity-1 transition-opacity" />
 					</div>
